573a89e2e56f1569ac04bf9e029df97f231b4ca7
james chalmers <james.chalmers184@gmail.com>
2022-07-03 19:22:25 +0100

    RadixSort algorithm and test added

---
+++ GithubCopilotProject/RadixSort.cs
@@ -0,0 +1,42 @@
+﻿
+namespace GithubCopilotProject
+{
+    public class RadixSort
+    {
+        public static void RadixSortAlgorithm(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            int exp = 1;
+            while (max / exp > 0)
+            {
+                int[] output = new int[array.Length];
+                int[] bucket = new int[10];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    bucket[(array[i] / exp) % 10]++;
+                }
+                for (int i = 1; i < 10; i++)
+                {
+                    bucket[i] += bucket[i - 1];
+                }
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    output[bucket[(array[i] / exp) % 10] - 1] = array[i];
+                    bucket[(array[i] / exp) % 10]--;
+                }
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = output[i];
+                }
+                exp *= 10;
+            }
+        }
+    }
+}
---
+++ SortingAlgorithmsTests/RadixSortTest.cs
@@ -0,0 +1,22 @@
+﻿using GithubCopilotProject;
+
+namespace SortingAlgorithmsTests
+{
+    public class RadixSortTest
+    {
+        [Fact]
+        public void RadixSortAlgorithmTest()
+        {
+            //Arrange
+            int[] array = new int[] { 5, 4, 3, 2, 1 };
+            //Act
+            RadixSort.RadixSortAlgorithm(array);
+            //Assert
+            Assert.Equal(1, array[0]);
+            Assert.Equal(2, array[1]);
+            Assert.Equal(3, array[2]);
+            Assert.Equal(4, array[3]);
+            Assert.Equal(5, array[4]);
+        }
+    }
+}
