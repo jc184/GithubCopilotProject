df5bf0efc58b87a6ab3b327e0d94d652f326110e
james chalmers <james.chalmers184@gmail.com>
2022-07-03 23:58:57 +0100

    CountingSort classes and methods added

--- GithubCopilotProject/Program.cs
+++ GithubCopilotProject/Program.cs
@@ -8,6 +8,7 @@ using static GithubCopilotProject.CombSort;
 using static GithubCopilotProject.BucketSort;
 using static GithubCopilotProject.BubbleSort;
 using static GithubCopilotProject.RadixSort;
+using static GithubCopilotProject.CountingSort;
 
 namespace GithubCopilotProject
 {
@@ -16,7 +17,7 @@ namespace GithubCopilotProject
         static void Main(string[] args)
         {
             int[] input = new int[] { 1, 4, 3, 2, 5, 8, 7, 9, 6, 10 };
-            RadixSortAlgorithm(input);
+            CountingSortAlgorithm(input);
             foreach (int i in input)
             {
                 Console.WriteLine(i);
